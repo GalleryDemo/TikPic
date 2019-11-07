6c33600befaab6a3b70f2a96a84e403b4fa43cc2
QING <qz761@outlook.com>
2019-11-07 20:44:24 +0800

    log added

--- app/src/main/java/com/oppo/tikpic/MainActivity.java
+++ app/src/main/java/com/oppo/tikpic/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("main", "onCreate: hyh");
+        Log.d("main", "onCreate: Qing");
     }
 }
