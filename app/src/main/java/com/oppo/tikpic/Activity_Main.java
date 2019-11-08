54b6c07dd7ad354f6759994253ebae9efe32f368
ollgithub <olltime@outlook.com>
2019-11-08 17:19:56 +0800

    Initial frame

---
+++ app/src/main/java/com/oppo/tikpic/Activity_Main.java
@@ -0,0 +1,117 @@
+package com.oppo.tikpic;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+public class Activity_Main extends AppCompatActivity {
+
+    private String TAG = "Activity_Main";
+    private Context mContext;
+
+    public DataManager data;
+    public int mScreenWidth, mScreenHeight, mScreenOrientation;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_main);
+        mContext = getApplicationContext();
+
+        getWindowInfo();
+
+
+        if (ContextCompat.checkSelfPermission(mContext, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                ActivityCompat.requestPermissions(Activity_Main.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+            }
+        } else {
+            begin();
+        }
+
+    }
+
+    private void begin() {
+        data = new DataManager();
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+        thread.start();
+        //replaceFragment();
+
+    }
+
+    public void replaceFragment(Fragment fragment) {
+        replaceFragment(fragment, true);
+    }
+
+    private void replaceFragment(Fragment fragment, boolean isInStack) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.mainLayout_FrameLayout, fragment);
+        if (isInStack) {
+            transaction.addToBackStack(null);
+        }
+        transaction.commit();
+    }
+
+    private void changeBar() {
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        //注意要清除 FLAG_TRANSLUCENT_STATUS flag
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(getResources().getColor(android.R.color.white));
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+
+        }
+
+    }
+
+    private void getWindowInfo() {
+        WindowManager mWindowManager = (WindowManager) getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics metrics = new DisplayMetrics();
+        mWindowManager.getDefaultDisplay().getMetrics(metrics);
+        mScreenWidth = metrics.widthPixels;
+        mScreenHeight = metrics.heightPixels;
+        mScreenOrientation = mWindowManager.getDefaultDisplay().getRotation();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(mContext, "存储空间读写权限申请成功", Toast.LENGTH_SHORT).show();
+                begin();
+            } else {
+                Toast.makeText(mContext, "存储空间读写权限申请失败", Toast.LENGTH_SHORT).show();
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                    ActivityCompat.requestPermissions(Activity_Main.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/oppo/tikpic/DataManager.java
@@ -0,0 +1,4 @@
+package com.oppo.tikpic;
+
+public class DataManager {
+}
