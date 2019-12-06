4841a425dec6f9528bd8c58257a4973c462405dc
80276658 <wangzihan1@oppo.com>
2019-12-06 19:50:32 +0800

    修改了LoadBitmap

--- app/src/main/java/com/demo/tikpic/timeline/TimelineFragment.java
+++ app/src/main/java/com/demo/tikpic/timeline/TimelineFragment.java
@@ -2,7 +2,6 @@ package com.demo.tikpic.timeline;
 
 import android.content.Context;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.Log;
@@ -60,7 +59,7 @@ public class TimelineFragment extends Fragment implements PhotoSection.ClickList
         final LoadPhotos loadPhotos = new LoadPhotos(hostActivity);
         final DataManager dataManager = DataManager.getInstance(hostActivity);
 
-        for(String albumName : dataManager.getAlbumKeySet()) {
+        for(String albumName : dataManager.getDateAlbumNames()) {
             Log.d(TAG, "onCreateView: " + albumName);
             sectionedAdapter.addSection(
                     new PhotoSection(albumName,
